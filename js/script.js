fdb871d04f3119e8a228b933b5756453add3f1a0
Renato Diniz Neto <renatodinizneto@gmail.com>
2024-08-03 15:19:15 -0300

    using js to animated the FAQ section on assurance page

--- js/script.js
+++ js/script.js
@@ -26,4 +26,25 @@ function ativarProdutos(parametro){
 parametros.forEach(ativarProdutos);
 
 
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button")
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle('ativa');
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas)
+
+
 
